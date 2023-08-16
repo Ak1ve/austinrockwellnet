62766b32ec92d7fc5b69ddbc56aea57dfaad7c36
Ak1ve <rockwell.austin04@gmail.com>
2023-08-16 16:15:21 -0400

    Rework

--- app/page.tsx
+++ app/page.tsx
@@ -1,57 +1,21 @@
-import Navbar from "@/components/navbar";
-import classNames from "classnames";
+import { EMAIl, INSTAGRAM, LINKEDIN, GITHUB, PHONE } from "@/components/assets";
 import Image from "next/image";
-
-
-function getColor(color: string) {
-    return {
-        "border-red-800 text-red-800 outline-red-800/30": color==="red",
-        "border-blue-800 text-blue-800 outline-blue-800/30": color==="blue",
-        "border-purple-800 text-purple-800 outline-purple-800/30": color==="purple",
-        "border-fuchsia-800 text-fuchsia-800 outline-fuchsia-800/30": color==="fuchsia",
-
-    };
-}
-
-function Section({ children, className, color="red" }: any) {
-    const clsName = classNames("border-2  shadow-lg rounded-2xl p-5 w-100 outline outline-offset-4", className,
-            getColor(color)
-        );
-    return <div className={clsName}>
-        {children}
-    </div>
-}
-
-function ImageCard({color, src, alt, className}: {color: string, src: string, alt: string, className?: string}) {
-    const clsName = classNames("rounded-full mx-auto border-2 outline outline-offset-4", getColor(color), className);
-    return (
-        <Image className={clsName} width="200" height="200" src={src} alt={alt} />
-    )
-
-}
-
-function WorkCard({src, children}: {src: string, children: any}) {
-
-}
 
 
 
 export default function Index() {
     return (
-        <div className="lg:grid lg:grid-cols-12 gap-10 container p-10 text-2xl">
-            <Section className="lg:col-span-4">
-            <h2>About Me</h2>
-            <div className="container p-3 mt-8">
-                <ImageCard src="/me.png" alt="A photo of me!" color="red" />
-                <h3 className="text-red-800/60 text-center mt-3">he/they</h3>
+        <div className="center">
+            <a href="/me.png" target="_blank"><Image height={200} width={200} src="/me.png" alt="A photo of me" className="photo" /></a>
+            <h3>Austin Rockwell</h3>
+            <div className="flex-break" />
+            <div className="socials">
+                <a href="https://www.instagram.com/magic.marinara/" target="_blank">{INSTAGRAM}</a>
+                <a href="https://www.linkedin.com/in/austin-rockwell-0593b527b/" target="_blank">{LINKEDIN}</a>
+                <a href="mailto:rockwell.austin04@gmail.com" target="_blank">{EMAIl}</a>
+                <a href="https://github.com/Ak1ve" target="_blank">{GITHUB}</a>
+                <a href="tel:419-706-5906">{PHONE}</a>
             </div>
-            </Section>
-            <Section className="lg:col-span-8" color="purple">
-                <h2 className="text-end">Experience</h2>
-                <ImageCard alt="Summit" src="/summit.png" color="purple" />
-                <ImageCard alt="Scene75" src="/scene75.png" color="purple" />
-                <ImageCard alt="FTMC" src="/ftmc.jpg" color="purple" />
-            </Section>
         </div>
     );
 }
--- next.config.js
+++ next.config.js
@@ -1,4 +1,22 @@
+const redirects = [
+    {
+        name: "Progress Book Grade Tester",
+        source: "/progressbook",
+        destination: "https://chrome.google.com/webstore/detail/progressbook-grade-tester/gbefacokacnljmbohkddpackecdcjflk",
+        permanent: false
+    },
+]
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    async redirects() {
+        return redirects.map((x) => {
+            let {name, ...rest} = x;
+            return rest;
+        })
+    }
+}
+
 
 module.exports = nextConfig
+

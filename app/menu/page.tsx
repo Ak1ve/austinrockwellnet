b1e1bbdc460ac243c61dd15340fb26cd5b1a7fa3
Ak1ve <rockwell.austin04@gmail.com>
2023-09-21 15:22:42 -0400

    why no worky

--- app/menu/page.tsx
+++ app/menu/page.tsx
@@ -1,13 +1,12 @@
 "use client";
 import { execSync } from "child_process";
-import path from "path";
 import { useState, useEffect } from "react";
 
 export default async function Index() {
     "use client";
     const [s, setS] = useState("");
     useEffect(() => {
-        fetch("api/python").then(x => {
+        fetch("https://www.austinrockwell.net/api/python").then(x => {
             x.text().then(x => setS(x));
         });
     });

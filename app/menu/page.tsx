1a206dece7dd935ccf5ac03aa16d107bdf07b3c9
Ak1ve <rockwell.austin04@gmail.com>
2023-09-21 15:32:01 -0400

    lol []

--- app/menu/page.tsx
+++ app/menu/page.tsx
@@ -4,14 +4,12 @@ import { useState, useEffect } from "react";
 
 export default async function Index() {
     "use client";
-    const [s, setS] = useState("");
+    const [s, setS] = useState("" as any);
     useEffect(() => {
         fetch("https://www.austinrockwell.net/api/python").then(x => {
             console.log(x);
             x.text().then(x => {setS(x); console.log(x)});
         });
-    });
-    console.log(s);
-    console.log("ELLO")
+    }, []);
     return <>MENU: {s}</>
 }

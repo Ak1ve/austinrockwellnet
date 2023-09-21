9fa1831882f07c7954587d420d6d4b025f0e7517
Ak1ve <rockwell.austin04@gmail.com>
2023-09-21 00:36:31 -0400

    testing

--- app/menu/page.tsx
+++ app/menu/page.tsx
@@ -2,6 +2,7 @@ import { execSync } from "child_process";
 
 export default async function Index() {
     async function doThing() {
+        "use server";
         execSync("python3 generate.py", {encoding: "utf-8"})
         return Math.random();
     }

083cc48d3625e53dd4fa35216af7d674411311fa
Ak1ve <rockwell.austin04@gmail.com>
2023-09-21 00:34:02 -0400

    testing

--- app/menu/page.tsx
+++ app/menu/page.tsx
@@ -2,7 +2,8 @@ import { execSync } from "child_process";
 
 export default async function Index() {
     async function doThing() {
-        return  execSync("python3 generate.py", {encoding: "utf-8"});
+        execSync("python3 generate.py", {encoding: "utf-8"})
+        return Math.random();
     }
     return <>{await doThing()}</>
 }

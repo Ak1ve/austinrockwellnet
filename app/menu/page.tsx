f07b6f2f18c37f53bb455d462ac359146aaf3305
Ak1ve <rockwell.austin04@gmail.com>
2023-09-21 14:28:44 -0400

    working on python things

--- app/menu/page.tsx
+++ app/menu/page.tsx
@@ -1,9 +1,16 @@
 import { execSync } from "child_process";
+import path from "path";
 
 export default async function Index() {
     async function doThing() {
         "use server";
-        execSync(`.\\venv\\bin\\python.exe .\\generate.py`, {encoding: "utf-8"})
+        const root = path.resolve(__dirname, "..\\..\\..\\..");
+        const python = root + "\\venv\\bin\\python.exe";
+        const script = root + "\\generate.py";
+        console.log(root);
+        console.log(python);
+        console.log(script);
+        // execSync(`${python} ${script}`, {encoding: "utf-8"})
         return Math.random();
     }
     return <>{await doThing()}</>

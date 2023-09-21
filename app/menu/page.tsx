73595a0098a0e1fb10d3491327d46cf60c17a8d8
Ak1ve <rockwell.austin04@gmail.com>
2023-09-21 14:31:21 -0400

    directory stuff

--- app/menu/page.tsx
+++ app/menu/page.tsx
@@ -4,9 +4,9 @@ import path from "path";
 export default async function Index() {
     async function doThing() {
         "use server";
-        const root = path.resolve(__dirname, "..\\..\\..\\..");
-        const python = root + "\\venv\\bin\\python.exe";
-        const script = root + "\\generate.py";
+        const root = path.resolve(__dirname, "../../../..");
+        const python = root + "/venv/bin/python.exe";
+        const script = root + "/generate.py";
         execSync(`${python} ${script}`, {encoding: "utf-8"})
         return Math.random();
     }

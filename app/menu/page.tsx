ae5f42f6fbb5324cf55a910fd27564f3eb795ade
Ak1ve <rockwell.austin04@gmail.com>
2023-09-21 15:28:23 -0400

    PLEASE WORK

--- app/menu/page.tsx
+++ app/menu/page.tsx
@@ -7,8 +7,11 @@ export default async function Index() {
     const [s, setS] = useState("");
     useEffect(() => {
         fetch("https://www.austinrockwell.net/api/python").then(x => {
-            x.text().then(x => setS(x));
+            console.log(x);
+            x.text().then(x => {setS(x); console.log(x)});
         });
     });
-    return <>{s}</>
+    console.log(s);
+    console.log("ELLO")
+    return <>MENU: {s}</>
 }

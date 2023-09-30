9223046ff006b385925dc6108e89a1d590f1a4a6
Ak1ve <rockwell.austin04@gmail.com>
2023-09-30 15:33:02 -0400

    requiresments

--- api/index.py
+++ api/index.py
@@ -1,19 +1,17 @@
+from __future__ import annotations
+from typing import Generic, TypeVar
 from flask import Flask, jsonify
-app = Flask(__name__)
 import dataclasses
 import datetime
 import io
-import pprint
 import re
 import PyPDF2 as Pdf
-import requests
+import asyncio
+import requests_async as req
 from pydantic import BaseModel, TypeAdapter
 
-
-
-_days = re.compile("(monday)|(tuesday)|(wednesday)|(thursday)|(friday)|(saturday)|(sunday)", flags=re.IGNORECASE)
-_form = re.compile("(dinner)|(lunch)", flags=re.IGNORECASE)
-
+app = Flask(__name__)
+T = TypeVar("T")
 
 
 class Food(BaseModel):
@@ -23,76 +21,95 @@ class Food(BaseModel):
     stationName: str
 
 
-def get_stations(txt: str):
-    # for stevie
-    foods = TypeAdapter(list[Food]).validate_json(txt)
-    d = {}
-    for food in foods:
-        name = food.stationName
-        y = d.get(name, "")
-        d[name] = y + food.name + ", "
-    return d
+@dataclasses.dataclass(slots=True)
+class Station:
+    name: str
+    menu: str
+
+    @classmethod
+    def from_text(cls, text: str) -> list[Station]:
+        foods = TypeAdapter(list[Food]).validate_json(text)
+        d = {}
+        for food in foods:
+            name = food.stationName
+            y = d.get(name, "")
+            d[name] = y + food.name + ", "
+        return [cls(name, food) for name, food in d.items()]
 
 
-def get_day(day: str):
-    # for stevie
-    root_link = "https://dish.avifoodsystems.com/api/menu-items?date={}&locationId=111&mealId={}"
-    # "breakfast": {station: food}, "lunch", "dinner"
-    breakfast = get_stations(requests.get(root_link.format(day, "182"), verify=False).text)
-    lunch = get_stations(requests.get(root_link.format(day, "183"), verify=False).text)
-    dinner = get_stations(requests.get(root_link.format(day, "184"), verify=False).text)
+@dataclasses.dataclass(slots=True)
+class DiningHalls:
+    clarity: str = ""
+    lord_saunders: str = ""
+    heritage: str = ""
 
-    return {
-        "breakfast": breakfast,
-        "lunch": lunch,
-        "dinner": dinner
-    }
+    @classmethod
+    def from_menus(cls, clarity: Menu[Day], heritage: Menu[Day], lord_saunders: Menu[Day]) -> Menu[Day[DiningHalls]]:
+        menu: Menu[Day[DiningHalls]] = Menu({
+            day: Day(lunch=DiningHalls(), dinner=DiningHalls()) for day in clarity.days
+        })
+        for day, m in clarity.days.items():
+            menu.days[day].lunch.clarity = m.lunch
+            menu.days[day].dinner.clarity = m.dinner
 
+        for day, m in heritage.days.items():
+            menu.days[day].lunch.heritage = m.lunch
+            menu.days[day].dinner.heritage = m.dinner
 
-def get_week():
-    # for stevie
-    now = datetime.datetime.now()
-    day = now.isoweekday()  # monday == 1
-    # MONDAY == 1
-    days = {
-        "monday": 1,
-        "tuesday": 2,
-        "wednesday": 3,
-        "thursday": 4,
-        "friday": 5,
-        "saturday": 6,
-        "sunday": 7,
-    }
-    return {
-        k: (now + datetime.timedelta(days=-(day - v))).strftime("%m/%d/%Y") for k, v in days.items()
-    }
+        for day, m in lord_saunders.days.items():
+            menu.days[day].lunch.lord_saunders = m.lunch
+            menu.days[day].dinner.lord_saunders = m.dinner
+        return menu
 
-
-def get_week_menu():
-    # for stevie
-    dates = get_week()
-    return {
-        day: get_day(date) for day, date in dates.items()
-    }
-
-
-
-@dataclasses.dataclass
-class Day:
-    lunch: str = ""
-    dinner: str = ""
-
-    def to_json(self):
+    def to_json(self) -> dict:
         return {
-            "lunch": self.lunch,
-            "dinner": self.dinner
+            "clarity": self.clarity,
+            "lord_saunders": self.lord_saunders,
+            "heritage": self.heritage
         }
 
 
-@dataclasses.dataclass
-class Menu:
-    name: str
-    days: dict[str, Day] = dataclasses.field(default_factory=lambda: {
+@dataclasses.dataclass(slots=True)
+class Response:
+    for_week: int
+    menu: Menu[Day[DiningHalls]]
+    stevie: Menu[list[Station]]
+
+    def to_json(self) -> dict:
+        return {
+            "for_week": self.for_week,
+            "menu": self.menu.to_json(),
+            "stevie": self.stevie.to_json()
+        }
+
+
+@dataclasses.dataclass()
+class Day(Generic[T]):
+    lunch: T = ""
+    dinner: T = ""
+    breakfast: T = None
+
+    def to_json(self):
+        def to(obj):
+            # last event is list[Station]
+            return obj if isinstance(obj, str) else None if obj is None\
+                else {s.name: s.menu for s in obj} if isinstance(obj, list) else obj.to_json()
+        lunch = to(self.lunch)
+        breakfast = to(self.breakfast)
+        dinner = to(self.dinner)
+        resp = {
+            "lunch": lunch,
+            "dinner": dinner
+        }
+        if breakfast is not None:
+            resp["breakfast"] = breakfast
+
+        return resp
+
+
+@dataclasses.dataclass(slots=True)
+class Menu(Generic[T]):
+    days: dict[str, T] = dataclasses.field(default_factory=lambda: {
         "monday": Day(),
         "tuesday": Day(),
         "wednesday": Day(),
@@ -105,83 +122,122 @@ class Menu:
     def to_json(self) -> dict:
         return {k: v.to_json() for k, v in self.days.items()}
 
-
-def get_menu(link) -> Menu:
-    thing = io.BytesIO(requests.get(link).content)
-    reader = Pdf.PdfReader(thing)
-    text = "\n".join(reader.pages[x].extract_text() for x in range(len(reader.pages)))
-    page = text.replace("  ", " ").replace("\n", " ").replace("*Gluten sensitive options available upon request", "")\
-        .replace("Vegetarian/Vegan", "").replace("Vegan", "").replace("Entrée", "")
-    pages = [x for x in _days.split(page) if x is not None]
-    menu = Menu(pages[0])
-    current_day = "monday"
-    for x in pages[1:]:
-        if _days.match(x):
-            current_day = x.lower()
-            continue
-        mode = "lunch" if "dinner" not in menu.name.lower() else "dinner"
-        for entry in [y.strip() for y in _form.split(x) if y is not None]:
-            if entry.lower() == "lunch":
-                mode = "lunch"
+    @classmethod
+    def from_pages(cls, name: str, pages: list[str], day_regex, form_regex) -> Menu[Day]:
+        self = cls()
+        current_day = "monday"
+        for x in pages:
+            if day_regex.match(x):
+                current_day = x.lower()
                 continue
-            if entry.lower() == "dinner":
-                mode = "dinner"
-                continue
-            if mode == "lunch":
-                menu.days[current_day].lunch += entry + " "
-            elif mode == "dinner":
-                menu.days[current_day].dinner += entry + " "
-    for day, obj in menu.days.items():
-        obj.lunch = re.sub(r"[ ]{2,}", " ", obj.lunch).strip()
-        obj.dinner = re.sub(r"[ ]{2,}", " ", obj.dinner).strip()
-    return menu
+            mode = "lunch" if "dinner" not in name.lower() else "dinner"
+            for entry in [y.strip() for y in form_regex.split(x) if y is not None]:
+                if (mode_entry := entry.lower()) in ["lunch", "dinner"]:
+                    mode = mode_entry
+                    continue
+                self.days[current_day].__dict__[mode] += entry + " "
+        for day, obj in self.days.items():
+            obj.lunch = re.sub(r"[ ]{2,}", " ", obj.lunch).strip()
+            obj.dinner = re.sub(r"[ ]{2,}", " ", obj.dinner).strip()
+        return self
 
 
-LAST_UPDATED = datetime.datetime.now() - datetime.timedelta(days=500)
-RESPONSE: dict = None
-
-
-def menus(w: int) -> dict[str, dict]:
-    clarity = get_menu(f"https://www.aviserves.com/Oberlin/menus/wk{w}/clarity_menu_wk{w}.pdf").to_json()
-    lord_saunders = get_menu(f"https://www.aviserves.com/Oberlin/menus/wk{w}/lord-saunders_menu_wk{w}.pdf").to_json()
-    heritage = get_menu(f"https://www.aviserves.com/Oberlin/menus/wk{w}/Heritage_menu_wk{w}.pdf").to_json()
-    
-    return {
-        day: {
-            "lunch": {
-                "clarity": clarity[day.lower()]["lunch"],
-                "lord_saunders": lord_saunders[day.lower()]["lunch"],
-                "heritage": heritage[day.lower()]["lunch"]
-            },
-            "dinner": {
-                "clarity": clarity[day.lower()]["dinner"],
-                "lord_saunders": lord_saunders[day.lower()]["dinner"],
-                "heritage": heritage[day.lower()]["dinner"]
-            }
-        } for day in ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"]
+class App:
+    _ISO_WEEK = {
+        "monday": 1,
+        "tuesday": 2,
+        "wednesday": 3,
+        "thursday": 4,
+        "friday": 5,
+        "saturday": 6,
+        "sunday": 7,
     }
-
-
-def fetch_menus() -> dict:
-    global RESPONSE
-    global LAST_UPDATED
-    now = datetime.datetime.now()
-    hours_since = (now - LAST_UPDATED).seconds / 60 / 60
-    # refreshes every day just in case
-    if hours_since <= 12 and RESPONSE is not None:
-        print("CACHE HIT")
-        return RESPONSE
-    
-    LAST_UPDATED = now
-    m = menus(now.isocalendar()[1])
-    RESPONSE = {
-        "for_week": now.isocalendar()[1],
-        "menu": m,
-        "stevie": get_week_menu()
+    _REPLACEMENTS = {
+        "\n": " ",
+        "*Gluten sensitive options available upon request": "",
+        "Vegetarian/Vegan": "",
+        "Vegan": "",
+        "Entrée": ""
     }
-    return RESPONSE
+    _days_regex = re.compile("(monday)|(tuesday)|(wednesday)|(thursday)|(friday)|(saturday)|(sunday)", flags=re.IGNORECASE)
+    _form_regex = re.compile("(dinner)|(lunch)", flags=re.IGNORECASE)
+    _stevie_root_link = "https://dish.avifoodsystems.com/api/menu-items?date={}&locationId=111&mealId={}"
+    _clarity_root_link = "https://www.aviserves.com/Oberlin/menus/wk{0}/clarity_menu_wk{0}.pdf"
+    _lord_saunders_root_link = "https://www.aviserves.com/Oberlin/menus/wk{0}/lord-saunders_menu_wk{0}.pdf"
+    _heritage_root_link = "https://www.aviserves.com/Oberlin/menus/wk{0}/Heritage_menu_wk{0}.pdf"
+
+    def __init__(self, *, cache_for_hours: int):
+        self.cache_for_hours = cache_for_hours
+        self._last_updated = None
+        self._response: dict | None = None
+
+    async def get_stevie(self, day: str) -> Day[Station]:
+        reqs = [req.get(self._stevie_root_link.format(day, day_num), verify=False) for day_num in ("182", "183", "184")]
+        breakfast, lunch, dinner = [Station.from_text(x.text) for x in await asyncio.gather(*reqs)]
+        return Day(breakfast=breakfast, lunch=lunch, dinner=dinner)
+
+    def get_week(self) -> dict[str, str]:
+        now = datetime.datetime.now()
+        day = now.isoweekday()
+        return {
+            k: (now + datetime.timedelta(days=-(day - v))).strftime("%m/%d/%Y") for k, v in self._ISO_WEEK.items()
+        }
+
+    async def get_stevie_week(self) -> Menu[Day[Station]]:
+        week = self.get_week()
+        reqs = [self.get_stevie(date) for date in week.values()]
+        return Menu({
+            day: menu for day, menu in zip(week.keys(), await asyncio.gather(*reqs))
+        })
+
+    def clean_menu_text(self, text: str):
+        txt = text
+        for key, value in self._REPLACEMENTS.items():
+            txt = txt.replace(key, value)
+        return txt
+
+    async def get_menu(self, link: str) -> Menu[Day[str]]:
+        reader = Pdf.PdfReader(io.BytesIO((await req.get(link, verify=False)).content))
+        text = "\n".join(reader.pages[x].extract_text() for x in range(len(reader.pages)))
+        page = self.clean_menu_text(text)
+        pages = [x for x in self._days_regex.split(page) if x is not None]
+        return Menu.from_pages(
+            name=pages[0],
+            pages=pages[1:],
+            day_regex=self._days_regex,
+            form_regex=self._form_regex
+        )
+
+    async def get_dining_halls(self) -> Menu[Day[DiningHalls]]:
+        w = datetime.datetime.now().isocalendar()[1]
+        dining_halls = [self.get_menu(self._clarity_root_link.format(w)),
+                        self.get_menu(self._heritage_root_link.format(w)),
+                        self.get_menu(self._lord_saunders_root_link.format(w))]
+        clarity, heritage, lord_saunders = await asyncio.gather(*dining_halls)  # NOQA
+        return DiningHalls.from_menus(clarity=clarity, heritage=heritage, lord_saunders=lord_saunders)
+
+    async def get_response(self) -> Response:
+        now = datetime.datetime.now()
+        dining_halls, stevie = await asyncio.gather(self.get_dining_halls(), self.get_stevie_week())
+        return Response(
+            for_week=now.isocalendar()[1],
+            menu=dining_halls,
+            stevie=stevie
+        )
+
+    async def fetch(self) -> dict:
+        now = datetime.datetime.now()
+        if self._response is None or (now - self._last_updated).seconds >= self.cache_for_hours * 60 * 60:
+            self._last_updated = now
+            self._response = jsonify((await self.get_response()).to_json())
+        else:
+            print("CACHE HIT")
+        return self._response
+
+
+client = App(cache_for_hours=12)
 
 
 @app.route("/api/menus")
-def hello_world():
-    return jsonify(fetch_menus())
+async def testing():
+    return await client.fetch()

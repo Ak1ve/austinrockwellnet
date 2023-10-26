942e10e78cc0f73e770e96b2e10f10d3ac590a86
Ak1ve <rockwell.austin04@gmail.com>
2023-10-26 14:32:31 -0400

    Some cleanup

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ function IconLink({ icon, children, href }: any) {
   return (
     <a className="hover:text-white hover:scale-105 flex items-center mt-5 lg:mt-0" href={href} target="_blank">
       <div className="mr-10">{icon}</div>
-      <div className="underline lg:hidden text-3xl">{children}</div>
+      <div className="underline lg:hidden text-3xl underline-offset-4">{children}</div>
     </a>
   )
 }
@@ -13,12 +13,12 @@ function IconLink({ icon, children, href }: any) {
 // red: #fd0d4d
 export default function Index() {
   return (
-    <div className="mx-auto text-[#fd0d4d] lg:translate-y-[50%]">
+    <div className="mx-auto text-[#fd0d4d] lg:translate-y-[50%] mb-10 lg:mb-0">
       <div className="lg:flex justify-between w-fit mx-auto gap-10">
         <a href="/me.png" target="_blank">
           <Image height={200} width={200} src="/me.png" alt="A photo of me" className="mx-auto mt-10 lg:mt-0 rounded-3xl border-[#fd0d4d] border-2" />
         </a>
-        <div className="text-7xl self-center">Austin Rockwell</div>
+        <div className="font-bold text-4xl lg:text-7xl self-center">Austin Rockwell</div>
       </div>
       <div className="lg:flex lg:justify-between lg:w-[60%] mt-10 mx-auto w-fit text-center">
         <IconLink href="https://www.instagram.com/magic.marinara/" icon={INSTAGRAM}>Instagram</IconLink>
@@ -28,7 +28,7 @@ export default function Index() {
         <IconLink href="tel:419-706-5906" icon={PHONE}>Text Me</IconLink>
         
         <div className="hover:text-white hover:scale-105 text-5xl font-serif mt-5 lg:mt-0">
-          <a className="underline lg:no-underline" href="/sobie">SOBIE</a>
+          <a className="underline lg:no-underline underline-offset-4" href="/sobie">SOBIE</a>
         </div>
       </div>
     </div>
